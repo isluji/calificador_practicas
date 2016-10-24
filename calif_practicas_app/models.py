f1056875ec1464d5d9b85416055c97576642c4dc
Ismael Luque Jiménez <isluji@gmail.com>
2016-10-24 18:46:45 +0200

    Añadidos tests unitarios (y corregido modelo Calificacion)

--- calif_practicas_app/models.py
+++ calif_practicas_app/models.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# To make readable URLs, we need to include a slug field in the Category model. First we need to
+# To make readable URLs, we need to include a slug field in the Enterprise model. First we need to
 # import the function slugify from Django that will replace whitespace with hyphens (for example,
 # "how do i create a slug in django" turns into "how-do-i-create-a-slug-in-django").
 from django.template.defaultfilters import slugify
@@ -12,12 +12,6 @@ from django.template.defaultfilters import slugify
 # You should be aware that Django creates an ID field for you automatically in each table relating to a model.
 # You therefore DO NOT need to explicitly define a primary key for each model.
 
-# def validar_rango_nota(value):
-#     if value < 0 or value > 10:
-#         raise ValidationError(
-#             _('%(value)s no es una nota valida (debe estar entre 0 y 10)'),
-#             params={'value': value},
-#         )
 
 ########################## MODELS ##########################
 
@@ -48,8 +42,15 @@ class Calificacion(models.Model):
     empresa = models.ForeignKey(Empresa, related_name='calificaciones')
     usuario = models.CharField(max_length=50)
     opinion = models.TextField()
-    nota = models.PositiveIntegerField(default=5)
-    #nota = models.PositiveIntegerField(default=5, validators[validar_rango_nota])
+    nota = models.PositiveSmallIntegerField(default=5)
+
+    def save(self, *args, **kwargs):
+        if self.nota < 0:
+            self.nota = 0
+        elif self.nota > 10:
+            self.nota = 10
+
+        super(Calificacion, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.usuario + ' -> ' + str(self.empresa) + ' -> ' + str(self.nota)
--- calif_practicas_app/tests.py
+++ calif_practicas_app/tests.py
@@ -1,6 +1,48 @@
 "Modulo para pruebas unitarias"
 
-
 from django.test import TestCase
+from models import Empresa, Calificacion
 
 # Create your tests here.
+class EmpresaTestCase(TestCase):
+    def setUp(self):
+        """Crea un par de objetos para realizar el test con ellos"""
+        Empresa.objects.create(nombre="Sun Microsystems", slug="sun-microsystems")
+        Empresa.objects.create(nombre="Google", slug="google")
+
+    def test_empresa_genera_slug(self):
+        """Las empresas que pasan este test se crean y generan su nombre 'slug' correctamente"""
+        sun = Empresa.objects.get(nombre="Sun Microsystems")
+        google = Empresa.objects.get(nombre="Google")
+
+        # Con esto, se genera la forma slug del nombre (todo en minuscula y separado por guiones,
+        # de forma que sea apto para utilizarse en URLs)
+        sun.save()
+        google.save()
+
+        self.assertEqual(sun.slug, "sun-microsystems")
+        self.assertEqual(google.slug, "google")
+
+class CalificacionTestCase(TestCase):
+    sun = Empresa.objects.get_or_create(nombre="Sun Microsystems", slug="sun-microsystems")[0]
+    google = Empresa.objects.get_or_create(nombre="Google", slug="google")[0]
+
+    def setUp(self):
+        """Crea algunas calificaciones para realizar el test con ellas
+        (hemos creado las empresas previamente como variables de clase, pues actuan de clave externa)"""
+        Calificacion.objects.create(empresa=self.sun, usuario="paco",
+                                    opinion="He desarrollado aplicaciones web con Node.js", nota=7)
+        Calificacion.objects.create(empresa=self.google, usuario="jose",
+                                    opinion="No me ha gustado nada la forma de trabajar en esta empresa", nota=-2)
+        Calificacion.objects.create(empresa=self.sun, usuario="jose",
+                                    opinion="Increible, ojala todas las empresas fueran asi", nota=14)
+
+    def test_calificacion_valida_nota(self):
+        "Comprobamos que las notas son truncadas al rango [0-10]"
+        califCorrecta = Calificacion.objects.get(empresa=self.sun, usuario="paco")
+        califMenor = Calificacion.objects.get(empresa=self.google, usuario="jose")
+        califMayor = Calificacion.objects.get(empresa=self.sun, usuario="jose")
+
+        self.assertEqual(califMenor.nota, 0)
+        self.assertEqual(califCorrecta.nota, 7)
+        self.assertEqual(califMayor.nota, 10)
